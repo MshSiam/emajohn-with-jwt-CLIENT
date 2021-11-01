bbae87de5ab5d1278d557a8a82d133d4dc5141a6
MshSiam <siyamkenway@gmail.com>
2021-11-01 16:21:33 +0600

    jwt

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,12 @@
-console.log(process.env);
+console.log(process.env)
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
+  apiKey: "AIzaSyDw4q_BwAsaZAhJhF8zzt5QYDl_teUu-pk",
+  authDomain: "ema-john-simple-d9e0e.firebaseapp.com",
+  projectId: "ema-john-simple-d9e0e",
+  storageBucket: "ema-john-simple-d9e0e.appspot.com",
+  messagingSenderId: "961987945497",
+  appId: "1:961987945497:web:2429a838d9edb1db507688"
+}
 
-export default firebaseConfig;
+export default firebaseConfig
