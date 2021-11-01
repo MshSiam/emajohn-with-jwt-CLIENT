2d8af65b154e940fd8fe644ffda76f1bbd59b1c6
MshSiam <siyamkenway@gmail.com>
2021-11-01 23:05:43 +0600

    order page added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import useAuth from "../../hooks/useAuth"
 
 const Orders = () => {
+  const { user } = useAuth()
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/orders?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setOrders(data))
+  }, [])
   return (
     <div>
-      <h1>This is Order Page</h1>
+      <h1>You have placed {orders.length} order.</h1>
+      <ul>
+        {orders.map((order) => (
+          <li key={order._id}>
+            {order.name} : {order.email}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
